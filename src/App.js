493136991c74b6be4f1d8729141bdc0d1154a62c
Gzim-DArdhishta <gzimdardhishta0gmail.com>
2022-01-25 00:07:27 +0100

    Finished the Home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Body from './Components/Body';
 import Header from './Components/Header/Header';
 
+
 // rfce for shortcut
 
 function App() {
   return (
     <div className='container'>
       <Header/>
-      <Body/>
+      <Body />
     </div>
   );
 }
--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
-function Body() {
+import './body.css'
+import ximi from './img/ximi.jpg';
+
+function Body(props) {
   return (
     <div>
-        <h1 style={{color: 'red'}}>Hello</h1>
+        <i className="far fa-snowflake snow"></i>
+        <main>
+
+            <div id='info'>
+                <p className='greeting'>Hello, I'm</p>
+                <h2 className='name'>Gzim Dardhishta</h2>
+                <p className='job'>Software Developer</p>
+                <p className="email"><i class="fas fa-envelope"></i>gzimdardhishta0@gmail.com</p>
+                <p className="phone-number"><i class="fas fa-phone"></i> +383 45 842 753</p>
+                <p className="location"><i class="fas fa-map-marker-alt"></i> Kosovo</p>
+            </div>
+
+            <div className="photo">
+                <img src={ximi} alt="" />
+            </div>
+        </main>
     </div>
   );
 }
